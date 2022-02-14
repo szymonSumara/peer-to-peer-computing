96cce1fc3e0ab8fe5fbd07e3e0c415975795c619
Szymon Sumara <szymon.sumara.zst@gmail.com>
2022-02-14 09:48:20 +0100

    fix logger

--- src/logging/eventLogger.ts
+++ src/logging/eventLogger.ts
@@ -7,27 +7,27 @@ export class EventLogger extends Logger {
         super(loggingStrategy, "EventLogger");
     }
     thisStartTask(blockNumber: number, hash: string){
-        this.loggingStrategy.log("Our node started block " + blockNumber + " for hash " + hash);
+        super.log("Our node started block " + blockNumber + " for hash " + hash);
     }
     thisEndTask(blockNumber: number, hash: string){
-        this.loggingStrategy.log("Our node finished block " + blockNumber + " for hash " + hash);
+        super.log("Our node finished block " + blockNumber + " for hash " + hash);
     }
     thisNewJob(hash: string){
-        this.loggingStrategy.log("Our node started new job for hash " + hash);
+        super.log("Our node started new job for hash " + hash);
     }
     thisEndJob(result: string ,hash: string){
-        this.loggingStrategy.log("Our node finished job for hash " + hash + " with result: " +  result);
+        super.log("Our node finished job for hash " + hash + " with result: " +  result);
     }
     otherStartTask(sender: string ,blockNumber: number, hash: string){
-        this.loggingStrategy.log("Node" + sender  + " started block " + blockNumber + " for hash " + hash);
+        super.log("Node" + sender  + " started block " + blockNumber + " for hash " + hash);
     }
     otherEndTask(sender: string , blockNumber: number, hash: string){
-        this.loggingStrategy.log("Node" + sender  + " finished block " + blockNumber + " for hash " + hash);
+        super.log("Node" + sender  + " finished block " + blockNumber + " for hash " + hash);
     }
     otherNewJob(sender: string ,hash: string){
-        this.loggingStrategy.log("Node" + sender  + " started job for hash " + hash);
+        super.log("Node" + sender  + " started job for hash " + hash);
     }
     otherEndJob(sender: string ,result: string, hash: string){
-        this.loggingStrategy.log("Node" + sender  + " finished job for hash " + hash + " with result: " + result);
+        super.log("Node" + sender  + " finished job for hash " + hash + " with result: " + result);
     }
 }
--- src/logging/logger.ts
+++ src/logging/logger.ts
@@ -1,17 +1,17 @@
 import {LoggingStrategy} from "./loggingStrategy";
 
 export abstract class Logger{
-    protected loggingStrategy : LoggingStrategy;
+    protected super : LoggingStrategy;
     protected loggerLabel : string;
 
 
     protected constructor(loggingStrategy : LoggingStrategy, loggerLabel : string) {
-        this.loggingStrategy = loggingStrategy;
+        this.super = loggingStrategy;
         this.loggerLabel = loggerLabel;
     }
 
     log( data : string ){
         const d = new Date();
-        this.loggingStrategy.log(`[${this.loggerLabel}][${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}:${d.getMilliseconds()}]  ${data}`)
+        this.super.log(`[${this.loggerLabel}][${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}:${d.getMilliseconds()}]  ${data}`)
     }
 }
